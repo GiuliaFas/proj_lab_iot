7f88452328dba7d340305889e0641d27d931fe07
GiuliaFas <86486192+GiuliaFas@users.noreply.github.com>
2021-07-19 10:37:09 +0000

    Create SmartLight.h

---
+++ SmartLight.h
@@ -0,0 +1,14 @@
+#ifndef SMART_LIGHT_MSG_H
+#define SMART_LIGHT_MSG_H
+
+typedef nx_struct smart_light_msg {
+  nx_uint16_t counter;
+  nx_uint16_t nodeID;
+  nx_uint16_t flag_led;
+} smart_light_msg_t;
+
+enum {
+  AM_SMART_LIGHT_MSG = 6,
+};
+
+#endif
